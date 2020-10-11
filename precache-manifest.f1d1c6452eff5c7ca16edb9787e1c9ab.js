b47177040be5fa421acd7fd08bd4f19b8a5864eb
Vòng Đạt Cường <vongdatcuong280599@gmail.com>
2020-10-11 17:00:32 +0700

    Updates

---
+++ precache-manifest.f1d1c6452eff5c7ca16edb9787e1c9ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abeec3d032423408b58dbf90da92883",
+    "url": "/tictactoe2/index.html"
+  },
+  {
+    "revision": "382aad90f9e5b2588489",
+    "url": "/tictactoe2/static/css/main.35c609e1.chunk.css"
+  },
+  {
+    "revision": "c35602a6eb87114a9588",
+    "url": "/tictactoe2/static/js/2.827857ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe2/static/js/2.827857ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382aad90f9e5b2588489",
+    "url": "/tictactoe2/static/js/main.5de756cd.chunk.js"
+  },
+  {
+    "revision": "ea5df3a99bb8176c2b1f",
+    "url": "/tictactoe2/static/js/runtime-main.f9be4569.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe2/static/media/logo.5d5d9eef.svg"
+  }
+]);
